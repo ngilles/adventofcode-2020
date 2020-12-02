3cb70e642fdd1786dd0e9a966d7b7a14cb1dc771
Nicolas Gilles <nicolas.gilles@gmail.com>
2020-12-02 10:55:09 +0100

    day 2

---
+++ day_02.py
@@ -0,0 +1,33 @@
+
+from utils import puzzle_input
+from functools import partial
+import timeit
+
+with puzzle_input(2) as f:
+    data = [l.strip() for l in f.readlines()]
+
+
+def parse_password(l: str):
+    rule, password = l.split(':')
+    password = password.strip()
+    counts, char = rule.split(' ')
+    lower, upper = counts.split('-')
+    upper = int(upper)
+    lower = int(lower)
+    return (char, (lower, upper)), password
+
+def validate_password_sled(rule, password: str):
+    char, (lower, upper) = rule
+    chars = password.count(char)
+    valid = lower <= chars <= upper
+    return valid
+
+def validate_password_toboggan(rule, password: str):
+    char, (lower, upper) = rule
+    valid = (password[lower - 1] == char) ^ (password[upper - 1] == char)
+    return valid
+
+parsed_data = [parse_password(l.strip()) for l in data]
+
+print(sum(1 for p in parsed_data if validate_password_sled(*p)))
+print(sum(1 for p in parsed_data if validate_password_toboggan(*p)))
