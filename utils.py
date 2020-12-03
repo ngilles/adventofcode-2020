2152c1a6904578b9847d41708d9a351e4c467dfa
Nicolas Gilles <nicolas.gilles@gmail.com>
2020-12-03 11:51:27 +0100

    fix utils allow puzzle_input() to easily use inline example

--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
 
 from pathlib import Path
 from contextlib import contextmanager
+import io
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 @contextmanager
-def puzzle_input(day: int, root: Path = Path('.puzzle_inputs')):
-    if root.exists and not root.is_dir:
+def puzzle_input(day: int, example: str = None, force_example=False, root: Path = Path('.puzzle_inputs')):
+    if root.exists() and not root.is_dir():
         raise Exception('.puzzle_inputs should be a directory!')
     
-    if not root.exists:
+    if not root.exists():
         root.mkdir()
 
     input_name = f'day_{day}.txt'
@@ -19,8 +20,13 @@ def puzzle_input(day: int, root: Path = Path('.puzzle_inputs')):
     input_path = root / input_name
 
     # automatic download? in the future!
+    if input_path.exists() and not force_example:
+        with open(input_path, 'r') as f:
+            yield f
+    elif example:
+        yield io.StringIO(example)
+    else:
+        raise Exception('Data not found, and no example provided')
 
-    with open(input_path, 'r') as f:
-        yield f
 
     
