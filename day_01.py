754ee44d246e828233091fe5dec216c8959690dd
Nicolas Gilles <nicolas.gilles@gmail.com>
2020-12-03 14:25:31 +0100

    clean up source files from pre-commit hooks

--- day_01.py
+++ day_01.py
@@ -1,14 +1,14 @@
+import timeit
+from functools import partial
 
 from utils import puzzle_input
-from functools import partial
-import timeit
 
 with puzzle_input(1) as f:
     data = [int(l.strip()) for l in f.readlines()]
 
 
 def find_sum_pair(data: list[int], total: int):
-    '''Find the pair of elements that add to the given sum. In linear time'''
+    """Find the pair of elements that add to the given sum. In linear time"""
 
     sorted_data = list(sorted(data))
     i = 0
@@ -16,10 +16,10 @@ def find_sum_pair(data: list[int], total: int):
 
     while True:
         if j == i:
-            raise ValueError('Not found!')
-        
+            raise ValueError("Not found!")
+
         current_sum = sorted_data[i] + sorted_data[j]
-        if  current_sum == total:
+        if current_sum == total:
             return sorted_data[i], sorted_data[j]
 
         if current_sum < total:
@@ -27,25 +27,27 @@ def find_sum_pair(data: list[int], total: int):
         elif current_sum > total:
             j -= 1
 
+
 def find_sum_triplet(data: list[int], total: int):
-    '''Naive implementation of to find triplets. Sufficient for AOC sized loads.'''
+    """Naive implementation of to find triplets. Sufficient for AOC sized loads."""
     data_len = len(data)
     for i in range(data_len - 2):
         for j in range(i + 1, data_len - 1):
             for k in range(j + 1, data_len):
-                current_sum  = data[i] + data[j] + data[k]
+                current_sum = data[i] + data[j] + data[k]
                 if current_sum == total:
                     return data[i], data[j], data[k]
-                if current_sum >  total:
+                if current_sum > total:
                     break
 
-    raise ValueError('Non found!')
+    raise ValueError("Non found!")
+
 
 def find_sum_triplet_fast(data: list[int], total: int):
-    '''Modified version of find_pairs that finds triplets.
+    """Modified version of find_pairs that finds triplets.
 
-       Should be much faster than the naive version.
-    '''
+    Should be much faster than the naive version.
+    """
     data_len = len(data)
     i = 0
     m = 1
@@ -53,7 +55,7 @@ def find_sum_triplet_fast(data: list[int], total: int):
 
     while (j - i) > 1:
         current_sum = data[i] + data[m] + data[j]
-        if current_sum <= total: # start move the middle bit to look for a solution
+        if current_sum <= total:  # start move the middle bit to look for a solution
             while (current_sum := data[i] + data[m] + data[j]) < total:
                 m += 1
             if current_sum == total:
@@ -64,7 +66,8 @@ def find_sum_triplet_fast(data: list[int], total: int):
         else:
             j -= 1
 
-    raise ValueError('Non found!')
+    raise ValueError("Non found!")
+
 
 data = list(sorted(data))
 a, b = find_sum_pair(data, 2020)
@@ -74,5 +77,5 @@ print(a * b)
 a, b, c = find_sum_triplet_fast(data, 2020)
 print(a, b, c, a * b * c)
 
-#print(timeit.timeit(partial(find_sum_triplet, data, 2020), number=10000))
-#print(timeit.timeit(partial(find_sum_triplet_fast, data, 2020), number=10000))
+# print(timeit.timeit(partial(find_sum_triplet, data, 2020), number=10000))
+# print(timeit.timeit(partial(find_sum_triplet_fast, data, 2020), number=10000))
--- day_02.py
+++ day_02.py
@@ -1,20 +1,21 @@
+import timeit
+from functools import partial
 
 from utils import puzzle_input
-from functools import partial
-import timeit
 
 with puzzle_input(2) as f:
     data = [l.strip() for l in f.readlines()]
 
 
 def parse_password(l: str):
-    rule, password = l.split(':')
+    rule, password = l.split(":")
     password = password.strip()
-    counts, char = rule.split(' ')
-    lower, upper = counts.split('-')
+    counts, char = rule.split(" ")
+    lower, upper = counts.split("-")
     upper = int(upper)
     lower = int(lower)
     return (char, (lower, upper)), password
+
 
 def validate_password_sled(rule, password: str):
     char, (lower, upper) = rule
@@ -22,10 +23,12 @@ def validate_password_sled(rule, password: str):
     valid = lower <= chars <= upper
     return valid
 
+
 def validate_password_toboggan(rule, password: str):
     char, (lower, upper) = rule
     valid = (password[lower - 1] == char) ^ (password[upper - 1] == char)
     return valid
+
 
 parsed_data = [parse_password(l.strip()) for l in data]
 
